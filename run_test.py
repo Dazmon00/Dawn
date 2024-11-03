9a8ed61bf8b9cd3eefdca1d891e33521da011263
Dazmon00 <dazmon00@gmail.com>
2024-11-03 21:35:53 +0800

    Create run_test.py

---
+++ run_test.py
@@ -0,0 +1,19 @@
+import asyncio
+import os
+import random
+from typing import Callable, Coroutine, Any, List, Set
+from dataclasses import dataclass
+
+# Assuming these modules are available or need to be adjusted for macOS:
+from loguru import logger
+from loader import config, semaphore, file_operations
+from core.bot import Bot
+from models import Account
+from utils import setup
+from console import Console
+from database import initialize_database
+
+
+
+if __name__ == "__main__":
+    print("test")
